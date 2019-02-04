a1a1a467ddbcde84e0e8ad44f4b135dc01125bf1
Daniel Valero Vicente <33459518+CurraheeDVV@users.noreply.github.com>
2019-02-04 08:48:27 +0100

    Add files via upload

---
+++ Ejemplo1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo1;
+
+/**
+ *
+ * @author daniel
+ */
+
+public class Ejemplo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    private int campo1;
+    private String nombre1;
+    public Ejemplo1(int campo1, String nombre1){
+        this.campo1 = campo1;
+        this.nombre1 = nombre1;
+        
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
