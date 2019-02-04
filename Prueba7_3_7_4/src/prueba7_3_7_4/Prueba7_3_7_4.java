c7e39533fa47849f6f494f44bf98ed54da8d7de3
daniel <daniel@PCdani>
2019-02-04 09:00:03 +0100

    Hola

---
+++ Prueba7_3_7_4/src/prueba7_3_7_4/Prueba7_3_7_4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba7_3_7_4;
+
+/**
+ *
+ * @author daniel
+ */
+public class Prueba7_3_7_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        // TODO code application logic here
+    }
+    
+}
